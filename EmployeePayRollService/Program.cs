be1102ee3e32826bc4b92b04b32baaf514c81a30
BashaVali <bashavali222@gmail.com>
2023-01-13 06:30:17 +0530

    Connecting With Data Base

---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,14 @@
+﻿using EmployeePayRollService;
+
+namespace EmployeePayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service");
+            EmployeePay pay = new EmployeePay();
+            EmployeeRepository.EmpDatabase();
+        }
+    }
+}
